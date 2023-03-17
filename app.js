471593466dd11c520c1f8b8785e8deeac36f5677
Aneepatil <anilspatil8@gmail.com>
2023-03-17 13:59:47 +0530

    changes

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json())
 app.use(cors())
 dotenv.config();
-const port = process.env.PORT
+const port = process.env.PORT || 8000
 mongoose
   .connect(process.env.MONGO_DB_URL)
   .then(() => console.log("MongoDB Connected Successfully....."))
